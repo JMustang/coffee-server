29914c25960ca4833e61cc6a8c6b7ca63a050ac6
Junior <eecfredes@gmail.com>
2025-10-30 21:42:44 -0300

    :rocket: application server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type Config struct {
 	Port string
 }
@@ -7,6 +16,22 @@ type Config struct {
 type Application struct {
 	config Config
 	// Models services.Models
+}
+
+func (app *Application) Serve() error {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("❌ Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+	fmt.Println("✅ API is listening on http://localhost:" + port)
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s", port),
+		// TODO: add router
+		// Handler: router.Router(),
+	}
+	return srv.ListenAndServe()
 }
 
 func main() {
