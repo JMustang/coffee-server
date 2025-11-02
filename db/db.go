89c236f216b0326e907a4c84c05d3830b2eff325
Junior <eecfredes@gmail.com>
2025-11-01 22:19:16 -0300

    :rocket:db connection

---
+++ db/db.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+	"database/sql"
+	"time"
+)
+
+type DB struct {
+	DB *sql.DB
+}
+
+var dbConn = &DB{}
+
+const maxOpenDbConn = 10
+const maxIdleDbConn = 5
+const maxDbLifetime = 5 * time.Minute
