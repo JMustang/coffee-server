8f70a665a90d7b277fd3b88977e5a4a2b47e9d8d
Junior <eecfredes@gmail.com>
2025-11-07 22:09:19 -0300

    :rocket: Get all coffee method

--- services/coffee.go
+++ services/coffee.go
@@ -1,6 +1,9 @@
 package services
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Coffee struct {
 	ID        string    `json:"id"`
@@ -12,4 +15,42 @@ type Coffee struct {
 	GrindUnit int16     `json:"grind_unit"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (c *Coffee) GetAllCoffees() ([]*Coffee, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `
+	SELECT id, name, image, roast, region, price, grind_unit, created_at, updated_at
+	FROM coffees
+	ORDER BY created_at DESC
+	`
+	rows, err := db.QueryContext(ctx, query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var coffees []*Coffee
+	for rows.Next() {
+		var coffee Coffee
+		err := rows.Scan(
+			&coffee.ID,
+			&coffee.Name,
+			&coffee.Image,
+			&coffee.Roast,
+			&coffee.Region,
+			&coffee.Price,
+			&coffee.GrindUnit,
+			&coffee.CreatedAt,
+			&coffee.UpdatedAt,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+		coffees = append(coffees, &coffee)
+	}
+	return coffees, nil
 }
