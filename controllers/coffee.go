374c7f90ddd07585c560356a4d07610c9d0f9a04
Junior <eecfredes@gmail.com>
2025-11-20 14:59:43 -0300

    :rocket: controller get coffee by id

--- controllers/coffee.go
+++ controllers/coffee.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/JMustang/coffee-server/helpers"
 	"github.com/JMustang/coffee-server/services"
+	"github.com/go-chi/chi/v5"
 )
 
 var coffee services.Coffee
@@ -18,6 +19,16 @@ func GetAllCoffees(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	helpers.WriteJSON(w, http.StatusOK, helpers.Envelope{"coffees": all})
+}
+
+func GetCoffeeById(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	coffee, err := coffee.GetCoffeeById(id)
+	if err != nil {
+		helpers.MessageLogs.ErrorLog.Println(err)
+		return
+	}
+	helpers.WriteJSON(w, http.StatusOK, coffee)
 }
 
 func CreateCoffee(w http.ResponseWriter, r *http.Request) {
